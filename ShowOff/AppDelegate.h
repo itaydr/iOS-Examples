8fe02f21ed7b594dd908e65a52fd146075c783a1
Itay Dressler <itaydressler@gmail.com>
2015-05-05 19:06:12 +0300

    Initial Commit

---
+++ ShowOff/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShowOff
+//
+//  Created by Itay Dressler on 5/5/15.
+//  Copyright (c) 2015 dressler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShowOff/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShowOff
+//
+//  Created by Itay Dressler on 5/5/15.
+//  Copyright (c) 2015 dressler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
