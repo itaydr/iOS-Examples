78970a582098c85aaa99eaecfacddc766248a92f
Itay Dressler <itaydressler@gmail.com>
2015-05-06 16:08:24 +0300

    Added Decay animation.

--- ShowOff/Controller/Image Animation/SOImageAnimationViewController.m
+++ ShowOff/Controller/Image Animation/SOImageAnimationViewController.m
@@ -19,6 +19,12 @@
 
 @implementation SOImageAnimationViewController
 
+#pragma mark Tweaks
+
++ (BOOL)isInDecayState {
+    return YES;
+}
+
 #pragma mark LifeCycle
 
 - (void)viewDidLoad
@@ -35,8 +41,7 @@
                                                                                  action:@selector(handlePan:)];
     
     CGFloat width               = self.view.bounds.size.width - 20.f;
-    CGFloat height              = roundf(width*kHeightFactor);
-    self.imageControl           = [[SOImageControl alloc] initWithFrame:CGRectMake(0, 0, width, height)];
+    self.imageControl           = [[SOImageControl alloc] initWithFrame:CGRectMake(0, 0, width, width)];
     self.imageControl .center   = self.view.center;
     [self.imageControl  setImage:[UIImage imageNamed:@"bg_1"]];
     [self.imageControl  addTarget:self action:@selector(touchDown:) forControlEvents:UIControlEventTouchDown];
@@ -74,7 +79,27 @@
 
 - (void)handlePan:(UIPanGestureRecognizer *)recognizer
 {
-    [self scaleDownView:recognizer.view];
+    
+    if ([SOImageAnimationViewController isInDecayState]) {
+        [self decay_handlePan:recognizer];
+    }
+    else {
+        [self scaleDownView:recognizer.view];
+        CGPoint translation = [recognizer translationInView:self.view];
+        recognizer.view.center = CGPointMake(recognizer.view.center.x + translation.x,
+                                             recognizer.view.center.y + translation.y);
+        [recognizer setTranslation:CGPointMake(0, 0) inView:self.view];
+        
+        if(recognizer.state == UIGestureRecognizerStateEnded) {
+            CGPoint velocity = [recognizer velocityInView:self.view];
+            [self animateDismissWithVelocity:velocity];
+        }
+    }
+}
+
+#pragma mark Decay
+
+- (void)decay_handlePan:(UIPanGestureRecognizer *)recognizer {
     CGPoint translation = [recognizer translationInView:self.view];
     recognizer.view.center = CGPointMake(recognizer.view.center.x + translation.x,
                                          recognizer.view.center.y + translation.y);
@@ -82,9 +107,15 @@
     
     if(recognizer.state == UIGestureRecognizerStateEnded) {
         CGPoint velocity = [recognizer velocityInView:self.view];
-        [self animateDismissWithVelocity:velocity];
+        POPDecayAnimation *positionAnimation = [POPDecayAnimation animationWithPropertyNamed:kPOPLayerPosition];
+        positionAnimation.delegate = self;
+        positionAnimation.velocity = [NSValue valueWithCGPoint:velocity];
+        [recognizer.view.layer pop_addAnimation:positionAnimation forKey:@"layerrPositionAnimation"];
     }
 }
+
+#pragma mark Pan
+
 
 - (void)animateDismissWithVelocity:(CGPoint)velocity {
     POPSpringAnimation *positionAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPosition];
@@ -105,6 +136,10 @@
     scaleAnimation.toValue              = [NSValue valueWithCGSize:CGSizeMake(1, 1)];
     scaleAnimation.springBounciness     = 10.f;
     [view.layer pop_addAnimation:scaleAnimation forKey:kScaleAnimationKey];
+    
+    POPSpringAnimation *corner          = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerCornerRadius];
+    corner.toValue              = @(10);
+    [view.layer pop_addAnimation:corner forKey:@"cornerRadius"];
 }
 
 - (void)scaleDownView:(UIView *)view
@@ -113,6 +148,10 @@
     scaleAnimation.toValue              = [NSValue valueWithCGSize:CGSizeMake(0.5, 0.5)];
     scaleAnimation.springBounciness     = 10.f;
     [view.layer pop_addAnimation:scaleAnimation forKey:kScaleAnimationKey];
+    
+    POPSpringAnimation *corner          = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerCornerRadius];
+    corner.toValue              = @(view.bounds.size.width / 2);
+    [view.layer pop_addAnimation:corner forKey:@"cornerRadius"];
 }
 
 - (void)pauseAllAnimations:(BOOL)pause forLayer:(CALayer *)layer
@@ -120,6 +159,24 @@
     for (NSString *key in layer.pop_animationKeys) {
         POPAnimation *animation = [layer pop_animationForKey:key];
         [animation setPaused:pause];
+    }
+}
+
+#pragma mark - POPAnimationDelegate
+
+- (void)pop_animationDidApply:(POPDecayAnimation *)anim
+{
+    
+    if ([SOImageAnimationViewController isInDecayState]) {
+        BOOL isDragViewOutsideOfSuperView = !CGRectContainsRect(self.view.frame, self.imageControl.frame);
+        if (isDragViewOutsideOfSuperView) {
+            CGPoint currentVelocity = [anim.velocity CGPointValue];
+            CGPoint velocity = CGPointMake(currentVelocity.x, -currentVelocity.y);
+            POPSpringAnimation *positionAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPosition];
+            positionAnimation.velocity = [NSValue valueWithCGPoint:velocity];
+            positionAnimation.toValue = [NSValue valueWithCGPoint:self.view.center];
+            [self.imageControl.layer pop_addAnimation:positionAnimation forKey:@"layerPositionAnimation"];
+        }
     }
 }
 
